2f088a2edf9b05855d1796c1fe4879a55f013f3f
kgv <kgv@users.noreply.github.com>
2026-05-17 15:12:55 +0300

    2

--- src/app/panes/source/view/table.rs
+++ src/app/panes/source/view/table.rs
@@ -194,14 +194,14 @@ impl TableView<'_> {
     ) -> PolarsResult<()> {
         match (row, column) {
             (row, top::INDEX) => {
-                // ui.label(row.to_string());
-                ui.label(format!(
-                    "{}/{}",
-                    row.to_string(),
-                    self.data_frame[INDEX]
-                        .as_materialized_series()
-                        .str_value(row)?
-                ));
+                ui.label(row.to_string())
+                    .try_on_hover_ui(|ui| -> PolarsResult<()> {
+                        let text = self.data_frame[INDEX]
+                            .as_materialized_series()
+                            .str_value(row)?;
+                        ui.label(text);
+                        Ok(())
+                    })?;
             }
             (row, bottom::ONSET) => {
                 ui.label(
